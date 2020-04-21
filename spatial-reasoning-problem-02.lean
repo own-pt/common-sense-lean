cd8eb9f5bb4f1f4060e3369e1fb1fe58b0d7eed8
Fredson <fredsonerd@hotmail.com>
2020-04-21 00:14:21 -0300

    Resolution of problems 2 and 3 from SpatialReasoning

--- spatial-reasoning-problem-02.lean
+++ spatial-reasoning-problem-02.lean
@@ -21,39 +21,8 @@ axiom a3 : ∀ OBJ1 OBJ2,
     (orientation OBJ1 OBJ2 Right) ↔ (orientation OBJ2 OBJ1 Left)
 
 /- axioms to be added -/
-axiom a4 : ∀ X Y Z R, (orientation X Y R) ∧ (orientation Y Z R)
-    → (orientation X Z R)
-axiom a5 : ∀ X Y Z, (orientation X Y Right) ∧ (orientation Z Y Left)
+axiom a4 : ∀ X Y Z, (orientation X Y Right) ∧ (orientation Z Y Left)
     → (orientation X Z Right)
 
 theorem x_is_on_the_right_of_z: orientation X Z Right :=
-begin
-
-end
-
-/-
-
-1. (not (orientation X Z Right)) [] negated_conjecture
-2. (forall (?VAR1 ?VAR2 ?VAR3)
-    (=> (and (orientation ?VAR3 ?VAR2 Left) (orientation ?VAR1 ?VAR2 Right))
-        (orientation ?VAR1 ?VAR3 Right))) [] kb_SUMO_3
-3. (forall (?VAR1 ?VAR2 ?VAR3)
-    (or (orientation ?VAR1 ?VAR3 Right)
-        (not (orientation ?VAR3 ?VAR2 Left))
-        (not (orientation ?VAR1 ?VAR2 Right)))) [2] ennf_transformation
-4. (forall (?VAR1 ?VAR2 ?VAR3)
-    (or (or (orientation ?VAR1 ?VAR3 Right)
-        (not (orientation ?VAR3 ?VAR2 Left)))
-        (not (orientation ?VAR1 ?VAR2 Right)))) [3] flattening
-5. (forall (?VAR1 ?VAR2 ?VAR3)
-    (or (or (orientation ?VAR2 ?VAR1 Right)
-    (not (orientation ?VAR1 ?VAR3 Left)))
-    (not (orientation ?VAR2 ?VAR3 Right)))) [4] cnf_transformation
-6. (forall (?VAR1) (or (not (orientation Z ?VAR1 Left))
-    (not (orientation X ?VAR1 Right)))) [1, 5] resolution
-7. (orientation Z Y Left) [] kb_SUMO_118
-8. (not (orientation X Y Right)) [6, 7] resolution
-9. (orientation X Y Right) [] kb_SUMO_117
-10. false [8, 9] subsumption_resolution
-
--/
+    by exact (a4 _ _ _) ⟨a1, a2⟩ 
